b2c2862515396740ac76bf0c3b424253ff1ea955
Jenna <jennahofseth@hotmail.com>
2024-11-25 12:57:39 -0500

    ETCE integration draft for exp_00
    TODO: currently the br.ota address is 10.99.0.100/16 - being the same as br.ctl, which is 10.99.100.24, just w/ a different port, may cause conflicts in control vs OTA operations. change to a different IP

---
+++ letce2_archive/docs/running_without_manager.md
@@ -0,0 +1,46 @@
+# Running Experiment-00 Without EMANE-Manager
+Since the first experiment (exp-00) in this repository does not rely on robot locations to calculate pathloss, it can be run without the intermediate EMANE-Manager script. This page describes the process to start up the shared platform in this manner.
+
+## Setting up the EMANE Experiment
+### Configuring EMANE
+By default, `exp-00` is configured to initialize with 3 nodes and the special Gateway node. Nodes can be added or removed by modifying the [experiment.cfg](../exp-00/experiment.cfg) file.
+
+The format of a node instantiation block is as follows:
+```
+[nem-#:precomputed-node]
+@id=#
+```
+Where # is replaced with the next available number (between 1-253, inclusive)
+
+### Building EMANE
+Once configured, the `run-emane.sh` file can be used to build the experiment with:
+```
+sudo ./run-emane.sh build 0
+```
+The experiment can be confirmed built by verifying the `exp-00` directory has a subdirectory for each NEM
+
+## Running EMANE
+The EMANE experiment can be run by envoking:
+```
+sudo ./run-emane.sh start 0
+```
+Each network node will start an LXC container that can be accessed via `ssh`.
+To access `nem-1` the command is simply `ssh nem-1`. NEMs with IDs between 1 and 15 have their access IP addresses stored in `/etc/hosts`. Each LXC inherets the host file system and authentication credentials so the LXC can be logged into with the same user account as the host
+
+## Running emanerelayd (ARGoS Interface Program)
+The EMANE relay program, `emanerelayd`, is by default configured to not run automatically on each NEM to allow for manual debugging. To start the program, it must be run on each NEM. The command must be run from inside each LXC and must be provided the ID number of the NEM currently logged into. The program can be run with the `-d, --daemonize` argument to run it disconnected from the currently terminal.
+
+Once the program is running, it will wait until ARGoS connects to it, then resume running. This connection occurs after ARGoS is signaled in the next step.
+
+## Bypassing EMANE-Manger
+During ARGoS startup, the EMANE-Manager is expected to signal that it is ready to begin. Since the EMANE-Manager can be bypassed due to not needing locations. ARGoS can be resumed by sending the appropriate signal once the above setup is complete.
+```
+kill -CONT ${ARGoS_PID}
+```
+
+## Shutting Down EMANE
+When ready to shutdown EMANE, the following command can be used:
+```
+sudo ./run-emane.sh stop 0
+```
+This will kill all EMANE processes and shutdown all created LXCs.
---
+++ letce2_archive/run-emane.sh
@@ -0,0 +1,215 @@
+#!/usr/bin/env bash
+
+CURR_EXP_RANGE=0-2
+
+usage()
+{
+    echo
+    echo " usage: run-emane build [BUILD OPTIONS]... EXPNUM"
+    echo "        run-emane start [START OPTIONS]... EXPNUM"
+    echo "        run-emane stop  [STOP OPTIONS]... EXPNUM "
+    echo "        run-emane clean [STOP OPTIONS]... EXPNUM "
+    echo "        run-emane help"
+    echo
+    echo " build options:"
+    # echo "  -n NUMBER     Sample build option"
+    echo
+    echo " start options:"
+    echo "  -s FILE       File to run systems experiment control"
+    echo "  -f FILE       EMANE-Manager log file"
+    echo "  -l LEVEL      EMANE-Manager log level [0-5]"
+    echo
+    echo " stop options:"
+    # echo "  -n NUMBER     Sample stop option"
+    echo
+    echo " clean options:"
+    # echo "  -n NUMBER     Sample stop option"
+    echo
+}
+
+if [ $EUID -ne 0 ]; then
+    echo "You must be root to run this script"
+    exit 1
+fi
+
+case "$1" in
+    build)
+        OPTIND=2
+        # build_opt=
+
+        # # process options
+        # while getopts ":n" opt; do
+        #     case $opt in
+        #         n)
+        #             build_opt=0
+        #         ;;
+        #         \?)
+        #             echo "Invalid option: -$OPTARG" >&2
+        #             exit 1
+        #         ;;
+        #     esac
+        # done
+
+        # Shift mode keyword (in this case: build) out
+        shift $((OPTIND - 1))
+
+        if [ $# -ne 1 ]; then
+            echo "Invalid number of parameters" >&2
+            exit 1
+        fi
+
+        shopt -s extglob
+        exp_id=$1
+
+        if [[ $exp_id != +([$CURR_EXP_RANGE]) ]]; then
+            echo "Invalid experiment ID: $exp_id"
+            exit 1
+        fi
+
+        echo "Build EMANE"
+
+        cd exp-0"$exp_id" || return
+        make
+
+    ;;
+    start)
+        OPTIND=2
+        sys_file=
+        log_file=
+        log_level=
+
+        # process options
+        while getopts ":s:f:l" opt; do
+            case $opt in
+                s)
+                    sys_file="-s $OPTARG"
+                ;;
+                f)
+                    log_file="-f $OPTARG"
+                ;;
+                l)
+                    log_level="-l $OPTARG"
+                ;;
+                \?)
+                    echo "Invalid option: -$OPTARG" >&2
+                    exit 1
+                ;;
+            esac
+        done
+
+        # Shift mode keyword (in this case: start) out
+        shift $((OPTIND - 1))
+
+        if [ $# -ne 1 ]; then
+            echo "Invalid number of parameters" >&2
+            exit 1
+        fi
+
+        shopt -s extglob
+        exp_id=$1
+
+        if [[ $exp_id != +([$CURR_EXP_RANGE]) ]]; then
+            echo "Invalid experiment ID: $exp_id"
+            exit 1
+        fi
+
+        echo "Start EMANE"
+
+        cd exp-0"$exp_id" || return
+        letce2 lxc start
+        sleep 5
+
+        echo "Start Manager"
+
+        # Go to emane-argos.py
+        echo emane-argos.py "$sys_file" "$log_file" "$log_level"
+    ;;
+    stop)
+        OPTIND=2
+        # stop_opt=
+
+        # # process options
+        # while getopts ":n" opt; do
+        #     case $opt in
+        #         n)
+        #             stop_opt=0
+        #         ;;
+        #         \?)
+        #             echo "Invalid option: -$OPTARG" >&2
+        #             exit 1
+        #         ;;
+        #     esac
+        # done
+
+        # Shift mode keyword (in this case: stop) out
+        shift $((OPTIND - 1))
+
+        if [ $# -ne 1 ]; then
+            echo "Invalid number of parameters" >&2
+            exit 1
+        fi
+
+        shopt -s extglob
+        exp_id=$1
+
+        if [[ $exp_id != +([$CURR_EXP_RANGE]) ]]; then
+            echo "Invalid experiment ID: $exp_id"
+            exit 1
+        fi
+
+        echo "Stop Manager"
+        # Assume this isn't getting called mid-experiment and ARGoS-EMANE has
+        # already quit cleanly
+
+        echo "Stop EMANE"
+        cd exp-0"$exp_id" || return
+        letce2 lxc stop
+
+    ;;
+    clean)
+        OPTIND=2
+        #clean_opt=
+
+        # process options
+        # while getopts ":n" opt; do
+        #     case $opt in
+        #         n)
+        #             clean_opt=0
+        #         ;;
+        #         \?)
+        #             echo "Invalid option: -$OPTARG" >&2
+        #             exit 1
+        #         ;;
+        #     esac
+        # done
+
+        # Shift mode keyword (in this case: clean) out
+        shift $((OPTIND - 1))
+
+        if [ $# -ne 1 ]; then
+            echo "Invalid number of parameters" >&2
+            exit 1
+        fi
+
+        shopt -s extglob
+        exp_id=$1
+
+        if [[ $exp_id != +([$CURR_EXP_RANGE]) ]]; then
+            echo "Invalid experiment ID: $exp_id"
+            exit 1
+        fi
+
+        echo "Clean EMANE"
+
+        cd exp-0"$exp_id" || return
+        make clean
+    ;;
+    help)
+        usage
+        exit 0
+    ;;
+    *)
+        usage
+        exit 1
+    ;;
+esac
